9933cf086e1446857f6ccb0899ff090cf82f59d0
Aamir Nathani <amirnathani9@gmail.com>
2022-05-07 12:26:51 +0530

    feat: added doctor listing page

---
+++ doctor-finder/src/pages/DoctorsListing/DoctorsListing.js
@@ -0,0 +1,7 @@
+export function DoctorsListing() {
+    return <>
+        <h1>
+            Doctors Listing Page
+        </h1>
+    </>
+};
---
+++ doctor-finder/src/pages/index.js
@@ -0,0 +1,2 @@
+export { Home } from "./Home/Home"
+export { DoctorsListing } from "./DoctorsListing/DoctorsListing"
