fea6b70839c5ce454608d739b48fdf23992e724e
Aamir Nathani <amirnathani9@gmail.com>
2022-05-07 18:38:53 +0530

    refactor the code

--- doctor-finder/src/components/Hero/Hero.js
+++ doctor-finder/src/components/Hero/Hero.js
@@ -7,7 +7,7 @@ export function Hero() {
         <h1 className="mb-2">Doctor Finder</h1>
         <p className="font-size-6 mb-4">Find Doctors in your City</p>
         <Link to="/doctors-listing">
-          <button className="btn primary-outline-shop-btn font-size-5 border-radius-2 px-8 py-4">
+          <button className="btn primary-outline-hero-btn font-size-5 border-radius-2 px-8 py-4">
             Book Appointment
           </button>
         </Link>
