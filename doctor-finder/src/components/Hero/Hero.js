01f0b0b83566ca9c2e515c2c82c62a6af0622e2a
Aamir Nathani <amirnathani9@gmail.com>
2022-05-07 13:30:37 +0530

    feat: added link to hero section button

--- doctor-finder/src/components/Hero/Hero.js
+++ doctor-finder/src/components/Hero/Hero.js
@@ -1,12 +1,17 @@
-import "./Hero.css"
+import { Link } from "react-router-dom";
+import "./Hero.css";
 export function Hero() {
-    return (<>
-        <section className="main-image-container section-center flex items-center justify-center flex-col border-radius-2 my-8">
+  return (
+    <>
+      <section className="main-image-container section-center flex items-center justify-center flex-col border-radius-2 my-8">
         <h1 className="mb-2">Doctor Finder</h1>
         <p className="font-size-6 mb-4">Find Doctors in your City</p>
+        <Link to="/doctors-listing">
           <button className="btn primary-outline-shop-btn font-size-5 border-radius-2 px-8 py-4">
             Book Appointment
           </button>
+        </Link>
       </section>
-    </>)
-};
+    </>
+  );
+}
